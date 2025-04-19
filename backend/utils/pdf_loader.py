cb22b2d884c8351d5ee0a9bc4260a5c79be79f30
KrishnaA4 <kollikrishna116@gmail.com>
2025-04-19 13:28:42 +0530

    removed all unwanted files

--- backend/utils/pdf_loader.py
+++
@@ -1,6 +0,0 @@
-from langchain_community.document_loaders import PyPDFLoader
-
-def load_pdf_as_documents(pdf_path: str):
-    loader = PyPDFLoader(file_path=pdf_path)
-    return loader.load()
- # returns List[Document] with rich metadata
--- backend/vector_store/chroma_setup.py
+++
@@ -1,4 +0,0 @@
-from chromadb import PersistentClient
-
-def get_chroma_client(persist_path="chroma_db"):
-    return PersistentClient(path=persist_path)
